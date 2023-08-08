9ab3a7c26c9327cc05672c96e5158faf391a6db4
satyam.mishra03 <satyam.mishra03@CHDSEZ341160L.ad.infosys.com>
2023-08-08 16:30:14 +0530

    Spring first devtool example

---
+++ bootDevtoolTest/src/test/java/com/devtool/example/BootDevtoolTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtool.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootDevtoolTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
