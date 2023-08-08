a1127ca6a9a682c3dd22dc201c0f4dff164387a9
satyam.mishra03 <satyam.mishra03@CHDSEZ341160L.ad.infosys.com>
2023-08-08 18:28:04 +0530

    Learn about the get/post/delete/put mapping

--- bootRestExample/src/main/java/com/rest/example/controller/RestTestController.java
+++ bootRestExample/src/main/java/com/rest/example/controller/RestTestController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -48,14 +52,43 @@ public class RestTestController {
 	 * These 2 examples are showing that how to get data of book with and without parameters
 	 */
 	
-	@GetMapping("/getAllBooks")
+	@GetMapping("/book")
 	public List<BookMdl> getAllBooks() {
 		return bookService.getAllBooks();
 	}
 
-	@GetMapping("/getSingleBook/{id}")
+	@GetMapping("/book/{id}")
 	public BookMdl getSingleBook(@PathVariable("id") int id) {
 		return bookService.getSingleBook(id);
+	}
+	
+	/*
+	 * @PostMapping is used to add the data
+	 */
+	
+	@PostMapping("/book")
+	public BookMdl addBook(@RequestBody BookMdl bookMdl) {
+		BookMdl addBook = this.bookService.addBook(bookMdl);
+		return addBook;
+	}
+	
+	/*
+	 * @DeleteMapping is used to delete the data
+	 */
+	
+	@DeleteMapping("/book/{id}")
+	public void deleteBook(@PathVariable("id") int id) {
+		this.bookService.deleteBook(id);
+	}
+	
+	/*
+	 * @PutMapping is used to update the data
+	 */
+	
+	@PutMapping("/book/{id}")
+	public BookMdl updateBook(@RequestBody BookMdl bookMdl, @PathVariable("id") int id) {
+		this.bookService.updateBook(bookMdl, id);
+		return bookMdl;
 	}
 
 }
--- bootRestExample/src/main/java/com/rest/example/service/BookService.java
+++ bootRestExample/src/main/java/com/rest/example/service/BookService.java
@@ -2,8 +2,11 @@ package com.rest.example.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.rest.example.entity.BookMdl;
 
@@ -14,8 +17,10 @@ public class BookService {
 
 	static {
 		bookLst.add(new BookMdl(11, "Java book", "James"));
-		bookLst.add(new BookMdl(108, "Ramayan", "Ram"));
-		bookLst.add(new BookMdl(1121, "Geeta", "Krishna"));
+		bookLst.add(new BookMdl(12, "Ramayan", "Ram"));
+		bookLst.add(new BookMdl(13, "Geeta", "Krishna"));
+		bookLst.add(new BookMdl(14, "satyam Book", "satyam"));
+		bookLst.add(new BookMdl(15, "Python Book", "Germany"));
 	}
 
 	public List<BookMdl> getAllBooks() {
@@ -23,10 +28,27 @@ public class BookService {
 	}
 
 	public BookMdl getSingleBook(int id) {
-
 		BookMdl bookMdl = bookLst.stream().filter(e -> e.getBookId() == id).findFirst().get();
-
 		return bookMdl;
+	}
+
+	public BookMdl addBook(BookMdl b) {
+		bookLst.add(b);
+		return b;
+	}
+
+	public void deleteBook(int id) {
+		bookLst = bookLst.stream().filter(e -> e.getBookId() != id).collect(Collectors.toList());
+	}
+
+	public void updateBook(BookMdl bookMdl, int id) {
+		bookLst = bookLst.stream().map(e -> {
+			if (e.getBookId() == id) {
+				e.setBookName(bookMdl.getBookName());
+				e.setBookAuther(bookMdl.getBookAuther());
+			}
+			return e;
+		}).collect(Collectors.toList());
 	}
 
 }
