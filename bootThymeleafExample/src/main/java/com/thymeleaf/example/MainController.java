bf7b42fa28259bead049919749f4a0a6087e4a19
satyam.mishra03 <satyam.mishra03@CHDSEZ341160L.ad.infosys.com>
2023-08-10 18:00:00 +0530

    thymeleaf condition part completed

--- bootThymeleafExample/src/main/java/com/thymeleaf/example/MainController.java
+++ bootThymeleafExample/src/main/java/com/thymeleaf/example/MainController.java
@@ -1,9 +1,11 @@
 package com.thymeleaf.example;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,6 +21,28 @@ public class MainController {
 		m.addAttribute("currentDate", new Date().toLocaleString());
 
 		return "about";
+	}
+
+	@GetMapping("/iterate")
+	public String iterate(Model m) {
+
+		List<String> namesLst = List.of("Ram", "Sita", "Lakghman", "Hanuman");
+
+		m.addAttribute("names", namesLst);
+
+		return "iterate";
+	}
+
+	@GetMapping("/conditions")
+	public String conditions(Model m) {
+
+		List<String> namesLst = List.of("Ram", "Sita", "Lakghman", "Hanuman");
+
+		m.addAttribute("flag", true);
+		m.addAttribute("gender", "F");
+		m.addAttribute("names", namesLst);
+
+		return "conditions";
 	}
 
 }
