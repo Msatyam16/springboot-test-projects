c8a1a480dddedd7ae9bdf01ebf3943132ed461ea
satyam.mishra03 <satyam.mishra03@CHDSEZ341160L.ad.infosys.com>
2023-08-24 19:19:40 +0530

    24/Aug/2023 - Complete home/singup page designing

--- smartContactManager/src/main/java/com/smart/controller/MainController.java
+++ smartContactManager/src/main/java/com/smart/controller/MainController.java
@@ -1,14 +1,11 @@
 package com.smart.controller;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.smart.dao.UserRepo;
 import com.smart.entity.Contact;
 import com.smart.entity.User;
@@ -19,18 +16,23 @@ public class MainController {
 	@Autowired
 	private UserRepo userRepo;
 
-	@RequestMapping("/home")
+	@RequestMapping("/")
 //	@ResponseBody
 	public String home(Model model) {
-		
+
 		model.addAttribute("title", "Home - smart contact manager");
 		return "home";
 	}
-	
+
 	@RequestMapping("/about")
 	public String about(Model model) {
-		
-		model.addAttribute("title", "Home - smart contact manager");
+		model.addAttribute("title", "About - smart contact manager");
 		return "about";
+	}
+	
+	@RequestMapping("/singup")
+	public String singup(Model model) {
+		model.addAttribute("title", "singup - smart contact manager");
+		return "singup";
 	}
 }
