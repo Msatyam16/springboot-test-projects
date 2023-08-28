acc0d4a29c81e39c091bc51d70a1df01d6fd4b9f
satyam.mishra03 <satyam.mishra03@CHDSEZ341160L.ad.infosys.com>
2023-08-28 20:51:57 +0530

    Add form control

--- smartContactManager/src/main/java/com/smart/controller/MainController.java
+++ smartContactManager/src/main/java/com/smart/controller/MainController.java
@@ -3,12 +3,13 @@ package com.smart.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import com.smart.dao.UserRepo;
-import com.smart.entity.Contact;
 import com.smart.entity.User;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
@@ -29,10 +30,27 @@ public class MainController {
 		model.addAttribute("title", "About - smart contact manager");
 		return "about";
 	}
-	
+
 	@RequestMapping("/singup")
 	public String singup(Model model) {
 		model.addAttribute("title", "singup - smart contact manager");
+		model.addAttribute("user", new User());
+		return "singup";
+	}
+
+	@PostMapping("/do_register")
+	public String doRegister(@ModelAttribute("user") User user,
+			@RequestParam(value = "aggrement", defaultValue = "false") boolean aggrement, Model model,
+			HttpSession session) {
+
+		user.setRole("user");
+		user.setEnabled("true");
+
+		model.addAttribute("user", user);
+
+		System.out.println(user);
+		System.out.println("aggrement " + aggrement);
+
 		return "singup";
 	}
 }
