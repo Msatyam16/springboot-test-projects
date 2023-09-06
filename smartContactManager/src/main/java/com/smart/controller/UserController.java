f909b16f4b005af702c0218ad85f038945bc49b2
satyam.mishra03 <satyam.mishra03@CHDSEZ341160L.ad.infosys.com>
2023-09-06 17:28:45 +0530

    duplicate the base.html template and get the user details using Principle class

--- smartContactManager/src/main/java/com/smart/controller/UserController.java
+++ smartContactManager/src/main/java/com/smart/controller/UserController.java
@@ -1,14 +1,30 @@
 package com.smart.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.smart.dao.UserRepo;
+import com.smart.entity.User;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+	@Autowired
+	private UserRepo userRepo;
+
 	@RequestMapping("/index")
-	public String dashboard() {
+	public String dashboard(Model model, Principal principle) {
+
+		User user = this.userRepo.getUserByUserName(principle.getName());
+		model.addAttribute("user", user);
+		
+		System.out.println(user);
+
 		return "normal/user_dashboard";
 	}
 
